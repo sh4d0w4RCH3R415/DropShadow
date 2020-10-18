a9422ab3dc0efb0e95be94a670143972f9d28f23
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2020-10-17 18:50:48 -0700

    Add project files.

---
+++ Tester/DropShadowTest/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DropShadow;
+
+namespace DropShadowTest
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			drpShdw = new DropShadowCS();
+			drpShdw.CreateWNDParams(CreateParams);
+			drpShdw.CreateDropShadow(this, this);
+		}
+
+		DropShadowCS drpShdw;
+	}
+}
